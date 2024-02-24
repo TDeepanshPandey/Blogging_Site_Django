61fa81ae1ff3985b568b1218a4e55755ad7c2f78
Deepansh Pandey <easypandey.dp@gmail.com>
2024-02-24 14:24:29 +0100

    Last Lecture Update

--- bloggingsite/bloggingsite/urls.py
+++ bloggingsite/bloggingsite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include('blogs.urls')),
     path("accounts/login", views.login, name='login'),
-    path("accounts/logout", views.logout, name='logout', kwargs={'next_page':'/'})
+    path("accounts/logout", views.logout, name='logout', kwargs={'next_page':'/'}) 
 ]
